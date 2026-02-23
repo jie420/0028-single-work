ddd0a4d2b74096f4e523ffa202be288641b9b214
jie420 <ucfnjl2@ucl.ac.uk>
2026-02-23 12:52:40 +0000

    Add base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/0028-single-work/",
   server: {
   watch: {
     usePolling: true
